a93ef5a5f9d1ccf44feb2a61372628cbf219fd42
Ziad Saab <ziad.saab@gmail.com>
2020-04-25 00:09:40 -0400

    restore events

--- sqlndc2/exercise.sh
+++ sqlndc2/exercise.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 docker run --name mng -d -v $PWD:/app mongo:4.2.6-bionic
-wget "https://github.com/ziad-saab/scenarios/raw/master/sqlndc2/assets/events.json.gz"
-gunzip events.json
+wget "https://github.com/ziad-saab/scenarios/raw/master/sqlndc2/events.json.gz"
+gunzip events.json.gz
 docker exec -i mng mongoimport --jsonArray /app/events.json
 docker exec -it mng mongo
