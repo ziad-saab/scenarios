f98fd75d91fd7f48c39dbbd00687b24bb28043ec
Ziad Saab <ziad.saab@gmail.com>
2020-04-26 13:55:48 -0400

    kick

--- querying-mongo/exercise.sh
+++ querying-mongo/exercise.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 docker run --name mng -d -v $PWD:/app mongo:4.2.6-bionic
-wget "https://github.com/ziad-saab/scenarios/raw/master/sqlndc2/events.json.gz"
+wget "https://github.com/ziad-saab/scenarios/raw/master/querying-mongo/events.json.gz"
 gunzip events.json.gz
 docker exec -i mng mongoimport --jsonArray /app/events.json
+pwd
+hostname
 echo 'docker exec -it mng mongo' > /root/mongo.sh
 chmod +x /root/mongo.sh
+ls -l /root
